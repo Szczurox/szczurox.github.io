638c17420a9e06c07edba44ac39a30da06a0e56a
Szczurox <58273015+Szczurox@users.noreply.github.com>
2022-12-16 00:14:42 +0100

    after a long fight
    after a long fight, window can be closed by pressing X and by pressing on it's file icon
    Window is now a class
    Few cosmetic cahnges

--- components/Window.tsx
+++ components/Window.tsx
@@ -6,58 +6,81 @@ import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 interface WindowProps {
   title?: string;
   icon?: string;
+  show?: boolean;
   children?: React.ReactNode;
 }
 
-export const Window: React.FC<WindowProps> = ({
-  title = "Window",
-  icon = "txt-file-icon.svg",
-  children,
-}) => {
-  const [handleWidth, setHandleWidth] = useState<number>(500);
-  const [handleHeight, setHandleHeight] = useState<number>(35);
+interface WindowStates {
+  show?: boolean;
+  handleWidth?: number;
+  handleHeight?: number;
+}
 
-  return (
-    <div className={styles.window}>
-      <Draggable
-        defaultPosition={{
-          x: 0,
-          y: 0,
-        }}
-        handle={"." + styles.window_handle}
-      >
-        <div
-          style={{
-            position: "fixed",
-            top: 30 + "%",
-            left: 30 + "%",
+export class Window extends React.Component<WindowProps, WindowStates> {
+  constructor(props: WindowStates) {
+    super(props);
+    this.state = {
+      show: false,
+      handleWidth: 500,
+      handleHeight: 35,
+    };
+  }
+
+  toggleShow = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return this.state.show ? (
+      <div className={styles.window}>
+        <Draggable
+          defaultPosition={{
+            x: 0,
+            y: 0,
           }}
+          handle={"." + styles.window_handle}
         >
           <div
-            className={styles.window_handle}
             style={{
-              width: handleWidth,
-              height: handleHeight,
+              position: "fixed",
+              top: 30 + "%",
+              left: 30 + "%",
             }}
           >
-            <img
-              src={icon}
-              style={{ maxHeight: handleHeight - 10 }}
-              className={styles.icon}
-            />
-            <p className={styles.title}>{title}</p>
+            <div
+              className={styles.window_handle}
+              style={{
+                width: this.state.handleWidth,
+                height: this.state.handleHeight,
+              }}
+            >
+              <img
+                src={this.props.icon}
+                style={{ maxHeight: this.state.handleHeight! - 10 }}
+                className={styles.icon}
+              />
+              <p className={styles.title}>{this.props.title}</p>
+              <img
+                src={"x-symbol.svg"}
+                style={{ maxHeight: this.state.handleHeight! - 15 }}
+                className={styles.x}
+                onClick={(e) => this.toggleShow()}
+              />
+            </div>
+            <div
+              className={styles.window_content_background}
+              style={{
+                width: this.state.handleWidth,
+                minHeight: this.state.handleHeight,
+              }}
+            >
+              <div className={styles.window_content}>{this.props.children}</div>
+            </div>
           </div>
-          <div
-            className={styles.window_content_background}
-            style={{
-              width: handleWidth,
-              minHeight: handleHeight,
-            }}
-          >
-            <div className={styles.window_content}>{children}</div>
-          </div>
-        </div>
-      </Draggable>
-    </div>
-  );
-};
+        </Draggable>
+      </div>
+    ) : null;
+  }
+}
